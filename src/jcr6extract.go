44e8438fd6ab619d3c1bd38155046d9dcc00e065
Tricky1975 <facebook@tbbs.nl>
2017-12-10 22:19:52 +0100

    extractor version

--- src/jcr6extract.go
+++ src/jcr6extract.go
@@ -1,3 +1,27 @@
+/*
+	JCR6CLI
+	Extractor
+	
+	
+	
+	(c) Jeroen P. Broks, 2017, All rights reserved
+	
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+		
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+		
+	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
+	to the project the exceptions are needed for.
+Version: 17.12.10
+*/
 package main
 
 import (
@@ -32,8 +56,8 @@ const Blink  = ansistring.A_Blink
 const Bright = ansistring.A_Bright
 
 func init(){
-mkl.Version("","")
-mkl.Lic("","")
+mkl.Version("JCR6 CLI (GO) - jcr6extract.go","17.12.10")
+mkl.Lic    ("JCR6 CLI (GO) - jcr6extract.go","GNU General Public License 3")
 }
 
 var autoyes bool
