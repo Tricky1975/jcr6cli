c37fb1d8241725cc1aab753b550a70070c0253aa
Tricky1975 <rachel.tricky@hotmail.com>
2017-12-19 14:57:30 +0100

    Errors in windows compile script fixed

--- compileme.bat
+++ compileme.bat
@@ -8,17 +8,17 @@ md bin
         echo Compiling
 	echo = Main
         go build -o bin/jcr6.exe src/jcr6.go
-        echo = Build
+        echo = Add
         go build -o bin/jcr6_add.exe src/jcr6add.go
 rem        go build -o bin/jcr6_delete.exe src/jcr6delete.go
         echo = List
         go build -o bin/jcr6_list.exe src/jcr6list.go
         echo = Type
         go build -o bin/jcr6_type.exe src/jcr6type.go
-	echo = Extract
+	    echo = Extract
         go build -o bin/jcr6_extract.exe src/jcr6extract.go
         echo = Convert
         go build -o bin/jcr6_convert.exe jcr6cli/src/jcr6convert
-	echo = Version
-	go build -o bin/jcr6_version.exe jcr6cli/src/jcr6version
+	   echo = Version
+	   go build -o bin/jcr6_version.exe src/jcr6version.go
        echo "Ready!"
