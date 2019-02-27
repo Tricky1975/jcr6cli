12b2f50bdd33d3bcd2ffb7e33b71cd31ca71e5cd
Tricky1975 <facebook@tbbs.nl>
2019-02-27 17:25:33 +0100

    vUpdate

--- src/jcr6.go
+++ src/jcr6.go
@@ -1,27 +1,27 @@
 // License Information:
 // 	JCR6 CLI
 // 	Main program
-//
-//
-//
+// 	
+// 	
+// 	
 // 	(c) Jeroen P. Broks, 2017, 2019
-//
+// 	
 // 		This program is free software: you can redistribute it and/or modify
 // 		it under the terms of the GNU General Public License as published by
 // 		the Free Software Foundation, either version 3 of the License, or
 // 		(at your option) any later version.
-//
+// 		
 // 		This program is distributed in the hope that it will be useful,
 // 		but WITHOUT ANY WARRANTY; without even the implied warranty of
 // 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // 		GNU General Public License for more details.
 // 		You should have received a copy of the GNU General Public License
 // 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+// 	
 // 	Please note that some references to data like pictures or audio, do not automatically
 // 	fall under this licenses. Mostly this is noted in the respective files.
-//
-// Version: 19.02.21
+// 		
+// Version: 19.02.27
 // End License Information
 
 package main
@@ -56,7 +56,7 @@ const Red    =ansistring.A_Red
 const Bright=ansistring.A_Bright
 
 func init(){
-mkl.Version("JCR6 CLI (GO) - jcr6.go","19.02.21")
+mkl.Version("JCR6 CLI (GO) - jcr6.go","19.02.27")
 mkl.Lic    ("JCR6 CLI (GO) - jcr6.go","GNU General Public License 3")
 }
 
