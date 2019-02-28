5e5ec725c70cd1296b971a4e8241aae988f726c6
Tricky1975 <facebook@tbbs.nl>
2019-02-28 20:21:50 +0100

    log = toxic

--- src/jcr6add.go
+++ src/jcr6add.go
@@ -4,7 +4,7 @@
 // 	
 // 	
 // 	
-// 	(c) Jeroen P. Broks, 2017, 2018, All rights reserved
+// 	(c) Jeroen P. Broks, 2017, 2018, 2019
 // 	
 // 		This program is free software: you can redistribute it and/or modify
 // 		it under the terms of the GNU General Public License as published by
@@ -17,35 +17,13 @@
 // 		GNU General Public License for more details.
 // 		You should have received a copy of the GNU General Public License
 // 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 	
+// 	Please note that some references to data like pictures or audio, do not automatically
+// 	fall under this licenses. Mostly this is noted in the respective files.
 // 		
-// 	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
-// 	to the project the exceptions are needed for.
-// Version: 18.11.02
+// Version: 19.02.28
 // End License Information
-/*
-	JCR6CLI
-	Add files to a JCR6 resource
-	
-	
-	
-	(c) Jeroen P. Broks, 2017, 2018, All rights reserved
-	
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-		
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-		You should have received a copy of the GNU General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-		
-	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
-	to the project the exceptions are needed for.
-Version: 18.06.13
-*/
+
 package main
 
 import(
@@ -104,7 +82,7 @@ func ERR(msg string, fatal ...bool) {
 }
 
 func WARNING(msg string) {
-	log.Print(ansistring.SCol("ERROR! ",Red,Blink)+ansistring.SCol(msg,Yellow,0))
+	fmt.Println(ansistring.SCol("ERROR! ",Red,Blink)+ansistring.SCol(msg,Yellow,0))
 	countwarnings++
 }
 
@@ -113,7 +91,7 @@ func plural(number int,nounsingle,nounplural string) string{
 }
 
 func init(){
-mkl.Version("JCR6 CLI (GO) - jcr6add.go","18.11.02")
+mkl.Version("JCR6 CLI (GO) - jcr6add.go","19.02.28")
 mkl.Lic    ("JCR6 CLI (GO) - jcr6add.go","GNU General Public License 3")
 }
 
