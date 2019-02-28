22911f749058c040607410a6efe9731fd385eaf3
Tricky1975 <facebook@tbbs.nl>
2019-02-28 20:21:05 +0100

    Imports Jeroen's eXtremely Simplistic Repeatative Character Compression Algorithm

--- src/imps/drv/drv.go
+++ src/imps/drv/drv.go
@@ -1,3 +1,28 @@
+// License Information:
+// 	JCR6 CLI
+// 	Driver list
+// 	
+// 	
+// 	
+// 	(c) Jeroen P. Broks, 2017, 2019
+// 	
+// 		This program is free software: you can redistribute it and/or modify
+// 		it under the terms of the GNU General Public License as published by
+// 		the Free Software Foundation, either version 3 of the License, or
+// 		(at your option) any later version.
+// 		
+// 		This program is distributed in the hope that it will be useful,
+// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 		GNU General Public License for more details.
+// 		You should have received a copy of the GNU General Public License
+// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 	
+// 	Please note that some references to data like pictures or audio, do not automatically
+// 	fall under this licenses. Mostly this is noted in the respective files.
+// 		
+// Version: 19.02.28
+// End License Information
 /*
 	JCR6 CLI
 	Driver list
@@ -29,6 +54,7 @@ import(
 	_ "trickyunits/jcr6/jcr6lzma"
 	_ "trickyunits/jcr6/jcr6lzw"
 	_ "trickyunits/jcr6/jcr6flate"
+	_ "trickyunits/jcr6/jcr6jxsrcca"
 //	_ "trickyunits/jcr6/jcr6xz"
 
 	// file formats
